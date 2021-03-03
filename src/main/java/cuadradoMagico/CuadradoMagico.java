c2fcaca90d97b855de1d953de47df6215d00ccec
daniel <danisimarroreigada@gmail.com>
2021-03-03 13:13:17 +0100

    creación del constructor (Rellena la matriz)

--- src/main/java/cuadradoMagico/CuadradoMagico.java
+++ src/main/java/cuadradoMagico/CuadradoMagico.java
@@ -5,10 +5,32 @@
  */
 package cuadradoMagico;
 
+import java.util.Random;
+
 /**
  *
  * @author NitroPc
  */
 public class CuadradoMagico {
+    
+    private int [][] matriz = new int [3][3];
+
+    public CuadradoMagico() {
+        
+        for(int i = 0;i<this.matriz.length;i++){
+            for(int j = 0; j<this.matriz[i].length;j++){
+                matriz[i][j]=numeroAleatorio();
+            }
+        }
+        
+    }
+    
+    public static int numeroAleatorio(){
+        
+        Random aleatorio = new Random();
+        
+        return aleatorio.nextInt(9-1+1)+1;
+    }
+    
     
 }
